2332590cc3a89c52a41b9e819c662b3a04e684d7
nadriha <hafez.nadhira@gmail.com>
2024-04-17 18:20:06 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/transactionsevice/TransactionseviceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.transactionsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionseviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/transactionsevice/TransactionseviceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.transactionsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionseviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
